08cba94c3ede07a2c514f1fab719fa63afd0af8c
Parker Thomson <latridell.services@gmail.com>
2025-07-01 13:19:23 -0400

    Clean up console logs

--- src/index.js
+++ src/index.js
@@ -17,6 +17,74 @@ const app = new PIXI.Application();
       }
 
 
+      let promiseChainActive = false;
+
+
+      function topPromise(){
+
+        return new Promise((resolve) => {
+
+        let topInt = setInterval(()=>{
+         if (circle.y <= 0 + 50) {
+            console.error("hit 50")
+            hitBorders.top = true;
+            renderBorderTop(borderTop, true);
+            clearInterval(topInt)
+            resolve();
+            return;
+            }
+          }, 10)
+        })
+      }
+
+
+      function bottomPromise(){
+        return new Promise((resolve) => {
+
+          let bottomInt = setInterval(()=>{
+            console.log("bottom tick")
+              if(circle.y >= app.renderer.height - 50){
+                hitBorders.bottom = true;
+                renderBorderBottom(borderBottom, true);
+                clearInterval(bottomInt)
+                resolve();
+                return;
+              }
+          }, 10)
+        })
+      }
+
+      function leftPromise(){
+        return new Promise((resolve) => {
+ 
+          let leftInt = setInterval(()=>{
+              if (circle.x <= 0 + 50) {
+                hitBorders.left = true;
+                renderBorderLeft(borderLeft, true);
+                clearInterval(leftInt);
+                resolve();
+                return;
+              }
+          }, 10)
+        })
+      }
+
+
+      function rightPromise(){
+        return new Promise((resolve) => {
+
+          let rightInt = setInterval(()=>{
+                if(circle.x >= app.renderer.width - 50){         
+                hitBorders.right = true;
+                renderBorderRight(borderRight, true);
+                clearInterval(rightInt);
+                resolve();
+                return;
+              }
+          }, 10)
+        })
+      }
+
       function renderBorderTop(border, change){
            if(!change){
             border.beginFill("#ff0000");
@@ -31,68 +99,65 @@ const app = new PIXI.Application();
             
            }
            app.stage.addChild(border);
-          }
+      }
 
           
-          function renderBorderBottom(border, change){
-           if(!change){
-            border.beginFill("#ff0000");
-            border.drawRect(0, 690, app.renderer.width, 10);
-            border.endFill();            
-            
-           }else{
-            border.clear();
-            border.beginFill("#0000ff");
-            border.drawRect(0, 690, app.renderer.width, 10);
-            border.endFill();            
-            
-           }
-           app.stage.addChild(border);
-          }
+      function renderBorderBottom(border, change){
+      if(!change){
+        border.beginFill("#ff0000");
+        border.drawRect(0, 690, app.renderer.width, 10);
+        border.endFill();            
+        
+      }else{
+        border.clear();
+        border.beginFill("#0000ff");
+        border.drawRect(0, 690, app.renderer.width, 10);
+        border.endFill();            
+        
+      }
+      app.stage.addChild(border);
+      }
 
           
-          function renderBorderLeft(border, change){
-           if(!change){
-            border.beginFill("#ff0000");
-            border.drawRect(0, 10, 10, app.renderer.height - 20);
-            border.endFill();            
-            
-           }else{
-            border.clear();
-            border.beginFill("#0000ff");
-            border.drawRect(0, 10, 10, app.renderer.height - 20);
-            border.endFill();            
-            
-           }
-           app.stage.addChild(border);
-          }
+      function renderBorderLeft(border, change){
+        if(!change){
+        border.beginFill("#ff0000");
+        border.drawRect(0, 10, 10, app.renderer.height - 20);
+        border.endFill();            
+        
+        }else{
+        border.clear();
+        border.beginFill("#0000ff");
+        border.drawRect(0, 10, 10, app.renderer.height - 20);
+        border.endFill();            
+        
+        }
+        app.stage.addChild(border);
+      }
 
-          function renderBorderRight(border, change){
-           if(!change){
-            border.beginFill("#ff0000");
-            border.drawRect(690, 10, 10, app.renderer.height - 20);
-            border.endFill();            
-            
-           }else{
-            border.clear();
-            border.beginFill("#0000ff");
-            border.drawRect(690, 10, 10, app.renderer.height - 20);
-            border.endFill();            
-            
-           }
-           app.stage.addChild(border);
-          }
+      function renderBorderRight(border, change){
+        if(!change){
+        border.beginFill("#ff0000");
+        border.drawRect(690, 10, 10, app.renderer.height - 20);
+        border.endFill();            
+        
+        }else{
+        border.clear();
+        border.beginFill("#0000ff");
+        border.drawRect(690, 10, 10, app.renderer.height - 20);
+        border.endFill();            
+        
+        }
+        app.stage.addChild(border);
+      }
 
-          let borderTop = new PIXI.Graphics();
-          
-          
-          let borderBottom = new PIXI.Graphics();
+      let borderTop = new PIXI.Graphics();
+                
+      let borderBottom = new PIXI.Graphics();
 
+      let borderLeft = new PIXI.Graphics();
 
-          let borderLeft = new PIXI.Graphics();
-
-
-          let borderRight = new PIXI.Graphics();
+      let borderRight = new PIXI.Graphics();
 
 
       await app
@@ -117,47 +182,29 @@ const app = new PIXI.Application();
           renderBorderRight(borderRight, false);
           
           
-          console.log("app.renderer.height = ", app.renderer.height);
-          console.log("app.renderer.width = ", app.renderer.width);
         })
         .then(() => {
           app.ticker.add((delta) => {
-            //what do we have this wrapped in a then chain?
+
 
             if (circle.x >= app.renderer.width - 50 || circle.x <= 0 + 50) {
-              if(circle.x >= app.renderer.width - 50){
-                
-                //right is hit                
-                hitBorders.right = true;
-                renderBorderRight(borderRight, true);
 
-
-              } else if (circle.x <= 0 + 50) {
-                //left is hit
-                hitBorders.left = true;
-                renderBorderLeft(borderLeft, true);
-              }
               xvelocity = -xvelocity;
-              // console.log("circle x = ", circle.x);
+
             }
             if (circle.y >= app.renderer.height - 50 || circle.y <= 0 + 50) {
-              
-              if(circle.y >= app.renderer.height - 50){
-                
-                hitBorders.bottom = true
-                renderBorderBottom(borderBottom, true);
-
-              } else {
-                
-                hitBorders.top = true;
-                renderBorderTop(borderTop, true);5
-
-              }
-
-                
-              
+            
               yvelocity = -yvelocity;
-              // console.log("circle y = ", circle.y);
+
+            }
+
+            if(!promiseChainActive){
+              promiseChainActive = true;
+              rightPromise().then((res)=>bottomPromise().then((res)=>leftPromise()).then((res)=>topPromise())).then((res)=>{
+                setTimeout(()=>{
+                  window.alert("All borders hit!")
+                },100)
+              });
             }
 
 
